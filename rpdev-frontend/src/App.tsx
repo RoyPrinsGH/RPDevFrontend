0091d790031ceee5fc9f0a9ffd9d94fa1b1d2aea
Roy Prins <133809422+RoyPrinsGH@users.noreply.github.com>
2023-12-31 14:36:54 +0100

    Add basic about me page

--- rpdev-frontend/src/App.tsx
+++ rpdev-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hub from "./pages/Hub";
+import AboutMe from "./pages/AboutMe";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="hub" Component={Hub} />
+        <Route path="about" Component={AboutMe} />
       </Routes>
     </BrowserRouter>
   );
---
+++ rpdev-frontend/src/pages/AboutMe.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AboutMe: React.FC = () => {
+    return (
+        <div className='page p-4 bg-blue-300/25 h-full w-full'>
+            <div className='header text-center'>
+                <h1 className='font-mono font-bold'>.RP.Dev</h1>
+                <hr className='h-0.5 bg-blue-700/50' />
+                <div className='nav flex justify-center'>
+                    <a href='/hub' className='m-2'>Hub</a>
+                    <a href='/about' className='m-2'>About Me</a>
+                    <a href='/portfolio' className='m-2'>Portfolio</a>
+                </div>
+            </div>
+            <div className='padding pb-32' />
+            <div className='content text-center'>
+                <h1>About Me</h1>
+                <br />
+                <p>Hi! My name is Roy Prins, and I am full-stack software engineer.</p>
+                <p>Welcome to my personal site!</p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutMe;
