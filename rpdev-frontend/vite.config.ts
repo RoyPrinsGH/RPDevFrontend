ee8630b03795b1f231c0771d7b1cd8cc7d0e716f
Roy Prins <133809422+RoyPrinsGH@users.noreply.github.com>
2024-01-04 22:24:43 +0100

    Configure Vite to export modules as chunks

--- rpdev-frontend/vite.config.ts
+++ rpdev-frontend/vite.config.ts
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'app/app-[name]-[hash].js',
+        chunkFileNames: 'app/module-[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash].[extname]`;
+        },
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'], // react
+          three: ['three'], // three
+        }
+      }
+    }
+  },
 })
